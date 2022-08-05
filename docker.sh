244b8604e418374773428a45a7f54ef6e71b6e28
Jeffrey Lau <jeffrey.lau@ribose.com>
2022-08-05 19:12:49 +0800

    docker.sh: Allow overriding CentOS version via $CENTOS_VERSION

--- docker.sh
+++ docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker run -it -v $(pwd):/usr/local/yum centos:7 bash
+CENTOS_VERSION="${1:-7}"
 
+docker run --platform linux/amd64 -it \
+	--workdir /usr/local/yum \
+	-v "$(pwd)":/usr/local/yum amd64/centos:"${CENTOS_VERSION}" bash
